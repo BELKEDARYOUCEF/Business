612e1437729748022d14438c962c89f91dc8edbf
BELKEDARYOUCEF <youcefbelkedar2@gmail.com>
2025-01-09 03:22:18 +0100

    next commit

---
+++ main.js
@@ -0,0 +1,22 @@
+function validationForm(event){
+    event.preventDefault();
+    
+    let usr = document.getElementById("UserName").value;
+    let pwd = document.getElementById("Password").value;
+
+    if (usr !== "Business Model") {
+        alert("Le pseudo est incorrect.");
+        return false;
+    }
+
+    if (pwd !== "Business Model") {
+        alert("Le mot de passe est incorrect.");
+        return false;
+    }
+
+    if (pwd === "Business Model" && usr === "Business Model") {
+        window.location.href = "./Page/BootCampMMG.html";
+    }
+
+    return true;
+}
